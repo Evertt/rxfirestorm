f4880528ed3d733824625820a4b1d67f7f3ec487
Evert van Brussel <ecvanbrussel@gmail.com>
2020-09-30 20:55:19 +0200

    Choose correct global

--- src/WeakRef.ts
+++ src/WeakRef.ts
@@ -4,7 +4,7 @@ interface WeakRef {
   }
 }
 
-const WeakRef: WeakRef = (globalThis as any).WeakRef || class<T extends Object> {
+const WeakRef: WeakRef = getGlobal().WeakRef || class<T extends Object> {
   static map = new WeakMap()
 
   constructor(object: T) {
@@ -17,3 +17,20 @@ const WeakRef: WeakRef = (globalThis as any).WeakRef || class<T extends Object> 
 }
 
 export default WeakRef
+
+function getGlobal(): any {
+  switch (true) {
+		case typeof globalThis === 'object' && !!globalThis:
+			return globalThis
+		case typeof self === 'object' && !!self:
+			return self
+		case typeof window === 'object' && !!window:
+			return window
+		case typeof global === 'object' && !!global:
+			return global
+		case typeof Function === 'function':
+      return (Function('return this')())
+    default:
+      return {}
+	}
+}
