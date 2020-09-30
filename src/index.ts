d970f226134b1e8ea30eabac6db8dc266bd6f004
Evert van Brussel <ecvanbrussel@gmail.com>
2020-09-30 19:44:42 +0200

    Please work

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import type { ModelQuery, ModelStore } from "./ModelQuery"
-import type { Props, PropsOptional, PropsRequired } from "./types"
-import type { CollectionQuery, CollectionStore } from "./CollectionQuery"
 import Model, { init } from "./Model"
 import { BelongsTo } from "./relations/BelongsTo"
 import { SubCollection } from "./relations/SubCollection"
+import type { ModelQuery, ModelStore } from "./ModelQuery"
+import type { Props, PropsOptional, PropsRequired } from "./types"
+import type { CollectionQuery, CollectionStore } from "./CollectionQuery"
 
 export default Model
 export { SubCollection, BelongsTo, init }
